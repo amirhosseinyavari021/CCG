69a87eab5ca6cf549905686e344312cbc973a4a2
AY021 <amirhosseinyavari61@gmail.com>
2025-08-01 17:51:05 +0330

    Delete client/src/osDetails.js

--- client/src/osDetails.js
+++
@@ -1,5 +0,0 @@
-export const osDetails = {
-  linux: { versions: ['Ubuntu 22.04', 'Debian 11', 'Fedora 38', 'Arch Linux'], clis: ['Bash', 'Zsh', 'Fish'] },
-  windows: { versions: ['Windows 11', 'Windows 10', 'Windows Server 2022'], clis: ['PowerShell', 'CMD'] },
-  macos: { versions: ['Sonoma (14)', 'Ventura (13)'], clis: ['Zsh', 'Bash'] }
-};
