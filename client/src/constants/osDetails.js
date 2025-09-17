ed8fc165a6c393c4f50857b3c749a10b8b45a2af
AY021 <amirhosseinyavari61@gmail.com>
2025-09-17 21:54:53 +0330

    Update osDetails.js

--- client/src/constants/osDetails.js
+++ client/src/constants/osDetails.js
@@ -1,5 +1,18 @@
 export const osDetails = {
-  linux: { versions: ['Ubuntu 22.04', 'Debian 11', 'Fedora 38', 'Arch Linux'], clis: ['Bash', 'Zsh', 'Fish'] },
-  windows: { versions: ['Windows 11', 'Windows 10', 'Windows Server 2022'], clis: ['PowerShell', 'CMD'] },
-  macos: { versions: ['Sonoma (14)', 'Ventura (13)'], clis: ['Zsh', 'Bash'] }
+  linux: {
+    versions: ['Ubuntu 24.04 LTS', 'Debian 12', 'Fedora 40', 'Arch Linux (Latest)'],
+    clis: ['Bash', 'Zsh', 'Fish']
+  },
+  windows: {
+    versions: ['Windows 11', 'Windows 10', 'Windows Server 2025'],
+    clis: ['PowerShell 7', 'CMD']
+  },
+  macos: {
+    versions: ['Sequoia (15)', 'Sonoma (14)'],
+    clis: ['Zsh', 'Bash']
+  },
+  other: {
+    versions: [], // No predefined versions for 'other'
+    clis: ['Bash', 'Zsh', 'PowerShell', 'CMD', 'Sh'] // A generic list of common shells
+  }
 };
